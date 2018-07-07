9bcd4204a28a6f24b4cba40b49090af1679e8044
Mitch Hankins <mitchhankins92@gmail.com>
2018-07-07 17:27:26 -0600

    Adding db connection

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const config = require('./config');
+
+mongoose.connect(config.mongoUri);
+mongoose.connection.once('open', () => {
+  console.log('Connected to Database');
+});
 
 const server = hapi.server({
   port: 5000,
